124b5f4e1fa508386c1bd213e5d133bf3e7f3ad3
Hee Jeong <yg9316@swu.ac.kr>
2023-03-07 15:11:48 +0900

    jdbc01 commit6   sql문 오라클로 보내는 실습

---
+++ jdbc01/자바DB연결/회원가입.java
@@ -0,0 +1,11 @@
+package 자바DB연결;
+
+public class 회원가입 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MemberDAO dao = new MemberDAO();
+		dao.insert(); //win6
+	}
+
+}
