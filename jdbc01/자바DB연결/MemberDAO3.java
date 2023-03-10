c9c71487e62421f9b10dfc37a4aad1b0c037cfb1
Hee Jeong <yg9316@swu.ac.kr>
2023-03-10 18:27:34 +0900

    화면DB연결 DAO VO ArrayList 실습3 - list() 추가

--- jdbc01/자바DB연결/MemberDAO3.java
+++ jdbc01/자바DB연결/MemberDAO3.java
@@ -4,21 +4,99 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import 화면DB연결.MemberVO;
 
 public class MemberDAO3 { //CRUD
 
+	public ArrayList<MemberVO> list() {
+		ResultSet rs = null; //항목명 + 결과 데이터를 담고 있는 테이블 
+		
+		//가방들 넣어줄 큰 컨테이너 역할을 부품이 필요!
+		//ArrayList<MemberVO> ==> MemberVO만 들어간 arraylist라는 의미
+		ArrayList<MemberVO> list = new ArrayList<>();
+		
+		MemberVO bag = null;
+		try {
+			// 1.오라클 11g와 연결한 부품 설정
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("1. 오라클과 자바 연결할 부품 설정 성공.");
+			Locale.setDefault(Locale.US); //맥 locale에러나신 분들만!!!
+			
+			// 2.오라클 11g에 연결해보자.(java --- oracle) 
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "system";
+			String password = "oracle";
+			Connection con = DriverManager.getConnection(url, user, password); //Connection
+			//String data = JOptionPane.showInputDialog("이름입력"); //String, 임아무개 
+			System.out.println("2. 오라클 연결 성공.");
+			
+			//ipaddress ==> InetAddress
+			//String url = "http://wwww.naver.com";
+			//URL u = new URL(url);
+			//자바는 부품조립식이여서,String에 넣은 문자열을 특정한 부품으로 인식하지 못함.
+			//특정한 부품으로 인식하려면 그 부품으로 만들어주어야 한다.
+			//SQL부품으로 만들어주어야 함.
+			//PreparedStatement가 SQL부품!!
+			
+			String sql = "select * from hr.MEMBER";
+			PreparedStatement ps = con.prepareStatement(sql); //PreparedStatement
+			//삭제!!!! ps.setString(1, id);
+			System.out.println("3. SQL문 부품(객체)으로 만들어주기 성공.");
+			
+			rs = ps.executeQuery(); //select문 전송시  
+			System.out.println("4. SQL문 오라클로 보내기 성공.");
+			while(rs.next()) { //검색결과가 있는지 여부는 rs.next() 
+				//true이면 있다라는 의미, false이면 없다라는 의미 
+				//1. 검색겨로가가 있으면, 
+				//System.out.println("검색결과 있음. 성공.");
+				//2. 각 컬럼에서 값들을 꺼내오자. 
+				String id2 = rs.getString(1); //id
+				String pw = rs.getString("pw"); //pw
+				String name = rs.getString(3); //name
+				String tel = rs.getString(4); //tel
+				//System.out.println(	id2 + " " 
+//									+ pw + " " 
+//									+ name + " " 
+//									+ tel);
+				//검색결과를 검색화면 UI부분을 주어야 함.
+				//3. 가방을 만들자.
+				bag = new MemberVO();
+				bag.setId(id2);
+				bag.setPw(pw);
+				bag.setName(name);
+				bag.setTel(tel);
+				
+				//4. list에 bag을 추가해주자.
+				list.add(bag);
+			}
+			//System.out.println(result);
+			ps.close();
+			rs.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//return id, pw, name, tel은 XXXXX!
+		//return 뒤에는 반드시 여러 데이터를 묶어서 리턴해주어야 함.
+		//검색결과가 있을 때는 bag에 데이터가 들어있음. 
+		//검색결과가 없을 때는 bag에 무엇이 들어있나? null
+		return list;
+		
+	}
+	
+	
 	//메서드를 만드는 것 ==> 메서드 정의(define)!
 	//메서드를 정의했다고 실행되는 것은 아니다.!
 	//메서드를 쓰는 것 ==> 메서드 호출(call)!
 	public MemberVO one(String id) {
 		ResultSet rs = null; //항목명 + 결과 데이터를 담고 있는 테이블 
-		MemberVO bag = null;
 		//기본형 정수/실수/문자/논리만 값으로 초기화
 		//나머지 데이터형(참조형) 주소가! 들어가 있음.
 		//참조형 변수를 초기화할 때는 null(주소가 없다라는 의미)
+		MemberVO bag = null;
 		try {
 			// 1.오라클 11g와 연결한 부품 설정
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -52,31 +130,30 @@ public class MemberDAO3 { //CRUD
 				//true이면 있다라는 의미, false이면 없다라는 의미 
 				System.out.println("검색결과 있음. 성공.");
 				String id2 = rs.getString(1); //id
-				String pw = rs.getString(2); //pw
+				String pw = rs.getString("pw"); //pw
 				String name = rs.getString(3); //name
 				String tel = rs.getString(4); //tel
-				System.out.println(id2 + " "
-								+ pw + " "
-								+ name + " "
-								+ tel);
-				//검색결과를 검색하면 UI부분을 줘야함.
+				System.out.println(	id2 + " " 
+									+ pw + " " 
+									+ name + " " 
+									+ tel);
+				//검색결과를 검색화면 UI부분을 주어야 함.
 				bag = new MemberVO();
 				bag.setId(id2);
 				bag.setPw(pw);
 				bag.setName(name);
 				bag.setTel(tel);
-			}
-			else {
-				System.out.println("검색결과 없음.");
+			}else {
+				System.out.println("검색결과 없음. 성공.");
 			}
 			//System.out.println(result);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//return id, pw, name, tel은 XXXXX
-		//return 뒤에는 반드시 여러 데이터를 묶어서 리턴해주어야함.
-		//검색결과가 있을 때는 bag에 데이터가 들어있음.
-		//검색결과가 없을 때는 bag에 무엇이 들어있을까? -> null if문 실행이 안되었기 때문에
+		//return id, pw, name, tel은 XXXXX!
+		//return 뒤에는 반드시 여러 데이터를 묶어서 리턴해주어야 함.
+		//검색결과가 있을 때는 bag에 데이터가 들어있음. 
+		//검색결과가 없을 때는 bag에 무엇이 들어있나? null
 		return bag;
 		
 	}
@@ -133,7 +210,7 @@ public class MemberDAO3 { //CRUD
 			// 1.오라클 11g와 연결한 부품 설정
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("1. 오라클과 자바 연결할 부품 설정 성공.");
-			Locale.setDefault(Locale.US); //맥 locale에러나신 분들만!!!
+			Locale.setDefault(Locale.US);
 			
 			// 2.오라클 11g에 연결해보자.(java --- oracle) 
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
@@ -157,7 +234,7 @@ public class MemberDAO3 { //CRUD
 			ps.setString(2, bag.getId());
 			System.out.println("3. SQL문 부품(객체)으로 만들어주기 성공.");
 			
-			result = ps.executeUpdate(); 
+			result = ps.executeUpdate(); //1
 			//insert, update, delete문만!! sql문 실행결과가 int
 			System.out.println("4. SQL문 오라클로 보내기 성공.");
 			if(result == 1) {
@@ -230,3 +307,4 @@ public class MemberDAO3 { //CRUD
 		return result;
 	}
 }
+
