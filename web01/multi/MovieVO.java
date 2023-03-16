91afb807b51513277684536923c8d7b7a0bd9e38
Hee Jeong <yg9316@swu.ac.kr>
2023-03-16 13:27:16 +0900

    Movie테이블 JSP실습 commit 2

---
+++ web01/multi/MovieVO.java
@@ -0,0 +1,44 @@
+package multi;
+
+public class MovieVO {
+	private String id;
+	private String title;
+	private String content;
+	private String location;
+	private String director;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	@Override
+	public String toString() {
+		return "MovieVO [id=" + id + ", title=" + title + ", content=" + content + ", location=" + location
+				+ ", director=" + director + "]";
+	}
+}
