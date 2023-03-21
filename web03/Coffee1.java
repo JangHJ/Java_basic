ceee932e778900d9e17aaeb1fa7ed406b301bdfa
Hee Jeong <yg9316@swu.ac.kr>
2023-03-21 13:06:41 +0900

    web03 싱글톤 실습 Commit 3

---
+++ web03/Coffee1.java
@@ -0,0 +1,19 @@
+package com.multi.www.web02;
+
+public class Coffee1 {
+	int price;
+	String name;
+	
+	public static Coffee1 coffee1;
+	
+	Coffee1(int price, String name) {
+		super();
+		this.price = price;
+		this.name = name;
+	}
+
+//	@Override
+//	public String toString() {
+//		return "Coffee1 [price=" + price + ", name=" + name + "]";
+//	}
+}
---
+++ web03/CoffeeMain1.java
@@ -0,0 +1,12 @@
+package com.multi.www.web02;
+
+public class CoffeeMain1 {
+	public static void main(String[] args)
+	{
+		Coffee1 c1 = new Coffee1(1000, "라떼");
+		Coffee1 c2 = new Coffee1(2000, "아메리카노");
+		
+		System.out.println(c1);
+		System.out.println(c2);
+	}
+}
